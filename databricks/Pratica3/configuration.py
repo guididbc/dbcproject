4fdb2e39d44aeb22269e3f19a67a266129750c35
guididbc <guididbc>
2022-10-04 23:27:14 +0000

    pratica3

---
+++ databricks/Pratica3/configuration.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %fs ls /FileStore/tables/pratica
+
+# COMMAND ----------
+
+file_path_orin = "/FileStore/tables/pratica/olist_orders_dataset.csv"
+file_path_dest = "/FileStore/tables/pratica/parquet"
+
+# COMMAND ----------
+
+
